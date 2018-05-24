07b1856e676b87645a8432d1bf1bb8779363bde3
Md Ariful Islam <aisonnet01@gmail.com>
2018-05-25 02:50:10 +0600

    generate  some recipe

---
+++ nginx/recipes/configure.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: nginx
+# Recipe:: configure
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
---
+++ nginx/recipes/remove.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: nginx
+# Recipe:: remove
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
