74f43bbc24b942ed8e6e3c942189bcc24a37b0ea
Md Ariful Islam <aisonnet01@gmail.com>
2018-06-01 05:53:07 +0600

    deploy issue fix

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -4,6 +4,7 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+
 app = search(:aws_opsworks_app).first
 app_path = "/mnt/web"
 
@@ -11,11 +12,22 @@ package "git" do
   options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "16.04"
 end
 
-# deploy git repo from opsworks app
 application app_path do
+  javascript "4"
+  environment.update("PORT" => "80")
+  environment.update(app["environment"])
+
   git app_path do
-    repository app['app_source']['url']
+    repository app["app_source"]["url"]
     revision app["app_source"]["revision"]
-    deploy_key app['app_source']['ssh_key']
   end
+
 end
+# deploy git repo from opsworks app
+#application app_path do
+#  git app_path do
+#    repository app['app_source']['url']
+#    revision app["app_source"]["revision"]
+#    deploy_key app['app_source']['ssh_key']
+#  end
+#end
